fc69f7adc2eb7a7199cb5f4fe80576b3f2fc0b16
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-07-09 19:16:40 +0200

    inserito il for loop con la sua condizione, e le condizioni per i multipli di 3 e 5

--- main.js
+++ main.js
@@ -6,8 +6,19 @@ Prima di partire a scrivere codice poniamoci qualche domanda:
 Come faccio a sapere se un numero è divisibile per?
 Abbiamo visto qualcosa di particolare che possiamo usare?*/
 
-//for Loop per stampare i numeri da 1 a 100
+//for Loop per stampare i numeri da 1 a 100 con incremento
 
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
+  //condizione per la quale se si tratta di un multiplo di 3 e 5 viene stampato FizzBuzz (prima lo avevo messo alla fine ma mettendolo alla fine non funzona, perché deve prima cercare i multipli di entrambi e poi se questa condizione non è veritiera passa alle altre due condizioni )
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  }
+  // cerca i multipli di 3 e gli stampa Fizz
+  else if (i % 3 === 0) {
+    console.log("Fizz");
+  }
+  // cerca i multipli di 5 e stampa Buzz
+  else if (i % 5 === 0) {
+    console.log("Buzz");
+  }
 }
